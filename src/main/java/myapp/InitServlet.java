89e5110d32cf9b1211cf8c3335f4e5724e77ed4d
i304783 <i304783@192.168.0.160>
2018-04-17 05:30:39 +0200

    kk

--- src/main/java/myapp/InitServlet.java
+++ src/main/java/myapp/InitServlet.java
@@ -2,15 +2,17 @@ package myapp;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.appengine.repackaged.com.google.api.client.json.JsonFactory;
-import com.google.appengine.repackaged.com.google.api.client.json.JsonParser;
-import com.google.appengine.repackaged.com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 
 public class InitServlet extends HttpServlet {
 	  @Override
@@ -23,15 +25,35 @@ public class InitServlet extends HttpServlet {
 	    
 	    try {
 		    InputStream productsStream = getServletContext().getResourceAsStream("/WEB-INF/products.json");
-		    		    
-		    JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+		    
+		    JsonReader jsonReader = new JsonReader(new InputStreamReader(productsStream));
+		    while(jsonReader.hasNext()){
+		        JsonToken nextToken = jsonReader.peek();
+		        System.out.println(nextToken);
 
-	        JsonParser parser = jsonFactory.createJsonParser(productsStream);
-	        	        
-	        while(parser.nextToken() != null){
-	        	resp.getOutputStream().println("jsonToken = " + parser.getCurrentToken() + "<br>");	            
-	        }	        
-	        
+		        if(JsonToken.BEGIN_OBJECT.equals(nextToken)){
+
+		            jsonReader.beginObject();
+
+		        } else if(JsonToken.NAME.equals(nextToken)){
+
+		            String name  =  jsonReader.nextName();
+		            System.out.println(name);
+
+		        } else if(JsonToken.STRING.equals(nextToken)){
+
+		            String value =  jsonReader.nextString();
+		            System.out.println(value);
+
+		        } else if(JsonToken.NUMBER.equals(nextToken)){
+
+		            long value =  jsonReader.nextLong();
+		            System.out.println(value);
+
+		        }
+		    }
+		    jsonReader.close();
+		    productsStream.close();
 	    } catch (Exception e) {
 	    	e.printStackTrace(new PrintStream(resp.getOutputStream()));
 	        //throw new RuntimeException("failed to load Instance json file " + resourcePath + ": " + e.getMessage(), e);
