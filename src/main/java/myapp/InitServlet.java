a0ea597a7b318ff5359cb7cb49d7878dfdb20974
i304783 <i304783@192.168.0.160>
2018-04-17 04:42:54 +0200

    Init

--- src/main/java/myapp/InitServlet.java
+++ src/main/java/myapp/InitServlet.java
@@ -1,10 +1,16 @@
 package myapp;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.repackaged.com.google.api.client.json.JsonFactory;
+import com.google.appengine.repackaged.com.google.api.client.json.JsonParser;
+import com.google.appengine.repackaged.com.google.api.client.json.jackson2.JacksonFactory;
 
 public class InitServlet extends HttpServlet {
 	  @Override
@@ -12,5 +18,28 @@ public class InitServlet extends HttpServlet {
 	      throws IOException {
 	    //resp.setContentType("text/plain");
 	    resp.getWriter().println("Initializing...");
+	    
+	    
+	    // clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(new FileInputStream(new File("WEB-INF/client_secrets.json"))));
+	    
+	    try {
+		    InputStream productsStream = getServletContext().getResourceAsStream("/WEB-INF/products.json");
+		    		    
+		    JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+
+	        JsonParser parser = jsonFactory.createJsonParser(productsStream);
+	        	        
+	        while(parser.nextToken() != null){
+	        	resp.getOutputStream().println("jsonToken = " + parser.getCurrentToken() + "<br>");	            
+	        }	        
+	        
+	    } catch (Exception e) {
+	    	e.printStackTrace(new PrintStream(resp.getOutputStream()));
+	        //throw new RuntimeException("failed to load Instance json file " + resourcePath + ": " + e.getMessage(), e);
+	    } catch (Throwable th) {
+	    	th.printStackTrace(new PrintStream(resp.getOutputStream()));
+	    }
+	    
+
 	  }
 }
