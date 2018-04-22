88b618abbe31f08a9420f5d722e34c458fae0bc1
i304783 <i304783@192.168.0.160>
2018-04-22 09:30:19 +0200

    a

--- src/main/java/myapp/AutosuggestServlet.java
+++ src/main/java/myapp/AutosuggestServlet.java
@@ -30,7 +30,7 @@ public class AutosuggestServlet extends HttpServlet {
 		
 		for(int i = 0; i < productList.size(); i++) {
 			Product product = productList.get(i);
-			sb.append("{\"sku\":" + product.getSku() + ",\"name\"" + product.getName() + "},");
+			sb.append("{\"sku\":" + product.getSku() + ",\"name\":\"" + product.getName() + "\"},");
 		}
 		
 		sb.append("]");
